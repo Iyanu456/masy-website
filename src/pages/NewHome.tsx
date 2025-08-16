72bea1fc94c3b0fcca40cdcac8c000a278bc1bcb
iyanu456 <oyerindei13@gmail.com>
2025-08-16 07:25:22 +0200

    ui improvements

--- src/pages/NewHome.tsx
+++ src/pages/NewHome.tsx
@@ -2,7 +2,7 @@
 
 export default function NewHome() {
   return (
-    <main className="w-full mx-auto space-y-16 pb-[12em]">
+    <main className="w-full mx-auto space-y-16 max-sm:space-y-8 pb-[12em]">
 
       {/* Section 1 */}
        <section className="mx-auto flex flex-col lg:grid grid-cols-2 w-[95%] lg:w-[76%] gap-[3em] lg:gap-6 place-items-center max-sm:py-[2em] py-[3em] lg:py-[2em]">
@@ -25,7 +25,7 @@ export default function NewHome() {
 
           {/* Column 2 */}
           <div className="grid w-full max-sm:place-items-center max-sm:-mt-4s mt-0">
-            <div className="grid rounded-2xl overflow-hidden w-[60%] max-sm:w-[70%] lg:w-fit h-full lg:mr-0 ml-auto mx-auto ">
+            <div className="grid rounded-2xl overflow-hidden w-[60%] max-sm:w-[95%] lg:w-fit h-full lg:mr-0 ml-auto mx-auto ">
               <img src="/image 1.png" className="w-[100%] h-[100%] object-cover lg:mr-0 ml-auto"></img>
             </div>
           </div>
@@ -37,7 +37,7 @@ export default function NewHome() {
             <img src="/image 2.png" className="mx-auto w-[55vw] max-sm:w-[95%]  lg:w-full" />
           </div>
           <div className="grid lg:mr-0 ml-auto my-auto mx-auto place-items max-sm:w-[95%] w-[55vw] lg:w-[90%]">
-            <h3 className="max-sm:text-xl text-3xl mb-4 font-semibold">About Us</h3>
+            <h3 className="max-sm:text-xl text-3xl mb-4 font-semibold max-sm:font-bold">About Us</h3>
             <p>{`At MASY consulting, we help students like you excel academically and confidently through expertly crafted writing and tutoring services.`}<br></br> <br></br> {`Whether you’re writing a thesis, struggling with an assignment, or preparing for a big exam — we’ve got your back with professional guidance, clear explanations, and reliable support.`}</p>
           </div>
        </section>
