f59c54ca92fa8edd73fe93a9619f7e50636e9ed4
iyanu456 <oyerindei13@gmail.com>
2026-01-21 11:37:23 +0100

    content updates - removed newsletter, updated Linkedin Links

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -5,7 +5,7 @@ export default function Footer() {
     <footer className="w-full bg-[#212738] text-white py-18 mt-10 max-md:mt-1">
       <div className="w-[95%] md:w-[85%] flex flex-col justify-center lg:w-[76%] mx-auto md:grid grid-cols-2 gap-4">
 
-        <div className="hidden max-md:block text-center mb-9 space-y-4 mx-auto w-[90%] lg:w-[80%]">
+        {/*<div className="hidden max-md:block text-center mb-9 space-y-4 mx-auto w-[90%] lg:w-[80%]">
           <h4 className="font-medium mb-2">Subscribe to our newsletter</h4>
           <div className="flex bg-white p-1 rounded-xl w-[100%]">
             <input
@@ -17,7 +17,7 @@ export default function Footer() {
               Subscribe
             </button>
           </div>
-        </div>
+        </div>*/}
 
         <div className="flex flex-col  max-md:text-center lg:grid grid-cols-2 gap-7">
         <div className="space-y-4">
@@ -37,7 +37,7 @@ export default function Footer() {
         </div>
         </div>
 
-        <div className="max-md:hidden block space-y-4 mr-0 mx-auto w-full lg:w-[80%]">
+        {/*<div className="max-md:hidden block space-y-4 mr-0 mx-auto w-full lg:w-[80%]">
           <h4 className="font-medium mb-2">Subscribe to our newsletter</h4>
           <div className="flex bg-white p-1 rounded-xl w-[100%]">
             <input
@@ -49,7 +49,7 @@ export default function Footer() {
               Subscribe
             </button>
           </div>
-        </div>
+        </div>*/}
       </div>
 
       <div className="mt-[2em] mb-2 flex max-md:flex-col-reverse justify-between w-[95%] md:w-[85%] lg:w-[76%] mx-auto">
@@ -62,7 +62,7 @@ export default function Footer() {
             <li className="mb-0 mt-auto text-gray-300">Follow us on:</li>
             <li><a href="https://www.facebook.com/share/19ZdFXwVRb/?mibextid=wwXIfr" ><img src="./Facebook.png" className='max-w-[24px] mb-0 mt-auto' /></a></li>
             <li><a href="https://www.instagram.com/masyconsulting?igsh=cmhobXFlajI0cDN3&utm_source=qr"><img src="./Instagram.png" className='max-w-[24px] mb-0 mt-auto' /></a></li>
-            <li><a href="https://www.linkedin.com/company/masy-consulting/"><img src="./LinkedIn.png" className='max-w-[24px] mb-0 mt-auto' /></a></li>
+            <li><a href="https://www.linkedin.com/company/masy-consultings/"><img src="./LinkedIn.png" className='max-w-[24px] mb-0 mt-auto' /></a></li>
           </ul>
         </div>
       </div>
